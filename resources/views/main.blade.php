f839c3e646948ffcf46a7249cc56d593cc0ad0b4
jaggesher <jaggesher14@gmail.com>
2017-09-09 13:55:21 +0600

    Adding basic view files

---
+++ resources/views/main.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    {{--Every child page should inject page title through section name title--}}
+    @include('partials.BasicPartials._head')
+    @yield('OuterInclude')
+    <style type="text/css">
+        #ContentOfBody{
+            bottom: 0;
+            left: 0;
+            position: relative;
+            right: 0;
+            top: 0;
+            min-height: 100vh;
+        }
+    </style>
+    </head>
+
+    <body>
+        @include('partials.BasicPartials._navigatio')
+        <hr>
+
+        <div id="ContentOfBody" class="container">
+            @yield('ContentOfBody')
+            <div class="row">
+               
+            </div>
+        </div>
+        <div class="container">
+             <div class="col-sm-6">
+                <div class="main-class1">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="main-class2">
+                    
+                </div>
+            </div>
+        </div>
+        <footer class="clearfix">
+            <div>
+                <a href="#" class="fa fa-facebook"></a>
+                <a href="#" class="fa fa-twitter"></a>
+                <a href="#" class="fa fa-linkedin"></a>
+                <a href="#" class="fa fa-youtube"></a>
+            </div>
+            <p><span class="glyphicon glyphicon-copyright-mark"></span> All Right Reserved.</p>
+            <p>Developed By. Nandan Sharker, Dept Of CSE University Of Rajsgahi. </p>
+        </footer>
+    </body>
+
+</html>
