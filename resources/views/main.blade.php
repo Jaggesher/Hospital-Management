bdbb143796e112b8a42e441a8374c33f4371da24
IAmMonmoy <iammonmoy@gmail.com>
2017-09-13 16:57:41 +0600

    copied homepage code to Home/home.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,42 +8,7 @@
 
     <body>
         @include('partials.BasicPartials._navigation')
-     
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img class= "img-responsive" src="{{asset('img/1.jpg')}}" alt="image1" style="width:100%;">
-                </div>
-
-                <div class="item">
-                    <img class= "img-responsive" src="{{asset('img/2.jpg')}}" alt="image2" style="width:100%;">
-                </div>
-                
-                <div class="item">
-                    <img class= "img-responsive" src="{{asset('img/3.jpg')}}" alt="image2" style="width:100%;">
-                </div>
-            </div>
-
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-        
-
+        @yield('Home')
         <div id="ContentOfBody" class="container">
             @yield('ContentOfBody')
             <div class="row">
